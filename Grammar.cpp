687764db5384375cad1157e07c084a26777bba6d
keddelzz <keddelzz@web.de>
2021-12-12 12:14:45 +0100

    Convert nfa to dfa
    Turn a nondeterministic finite automaton into a deterministic one by
    subset construction. The main algorithm seems to work. We need a way
    to unit test it.
    
    There are several things to work on next. We have to remember which
    state is the implicitly constructed explicit error state. And for each
    accepting state we have to remember which token it accepts.

--- Grammar.cpp
+++ Grammar.cpp
@@ -8,11 +8,25 @@
 
 #include <core/Arena.h>
 #include <core/Formatting.h>
+#include <core/Map.h>
+#include <core/Set.h>
 #include <sigil/Nfa.h>
 #include <sigil/RegExp.h>
 #include <sigil/RegexParser.h>
 
 namespace sigil {
+
+nfa::State *start_state(nfa::Automaton &nfa)
+{
+    nfa::State *result = nullptr;
+    for (auto &state : nfa.states()) {
+        if (state->start) {
+            assert(result == nullptr and "Multiple start states");
+            result = state;
+        }
+    }
+    return result;
+}
 
 struct INfa
 {
@@ -32,8 +46,7 @@ static void epsilon_arc(nfa::Automaton &automaton, nfa::State *a, nfa::State *b)
     arc->epsilon = true;
 }
 
-static INfa create_regex_automaton(
-    nfa::Automaton &automaton, const RegExp *regexp)
+static INfa create_regex_nfa(nfa::Automaton &automaton, const RegExp *regexp)
 {
     auto start = automaton.create_state();
     start->start = true;
@@ -51,10 +64,10 @@ static INfa create_regex_automaton(
         case RegExp::Type::Alternative: {
             const auto exp = reinterpret_cast<const Alternative *>(regexp);
 
-            auto left = create_regex_automaton(automaton, exp->left());
+            auto left = create_regex_nfa(automaton, exp->left());
             drop_config(left);
 
-            auto right = create_regex_automaton(automaton, exp->right());
+            auto right = create_regex_nfa(automaton, exp->right());
             drop_config(right);
 
             epsilon_arc(automaton, start, left.start);
@@ -66,10 +79,10 @@ static INfa create_regex_automaton(
         case RegExp::Type::Concatenation: {
             const auto exp = reinterpret_cast<const Concatenation *>(regexp);
 
-            auto left = create_regex_automaton(automaton, exp->left());
+            auto left = create_regex_nfa(automaton, exp->left());
             drop_config(left);
 
-            auto right = create_regex_automaton(automaton, exp->right());
+            auto right = create_regex_nfa(automaton, exp->right());
             drop_config(right);
 
             epsilon_arc(automaton, start, left.start);
@@ -80,7 +93,7 @@ static INfa create_regex_automaton(
         case RegExp::Type::Kleene: {
             const auto exp = reinterpret_cast<const Kleene *>(regexp);
 
-            auto wrapped = create_regex_automaton(automaton, exp->exp());
+            auto wrapped = create_regex_nfa(automaton, exp->exp());
             drop_config(wrapped);
 
             epsilon_arc(automaton, start, wrapped.start);
@@ -92,7 +105,7 @@ static INfa create_regex_automaton(
         case RegExp::Type::PositiveKleene: {
             const auto exp = reinterpret_cast<const PositiveKleene *>(regexp);
 
-            auto wrapped = create_regex_automaton(automaton, exp->exp());
+            auto wrapped = create_regex_nfa(automaton, exp->exp());
             drop_config(wrapped);
 
             epsilon_arc(automaton, start, wrapped.start);
@@ -103,7 +116,7 @@ static INfa create_regex_automaton(
         case RegExp::Type::Optional: {
             const auto exp = reinterpret_cast<const Optional *>(regexp);
 
-            auto wrapped = create_regex_automaton(automaton, exp->exp());
+            auto wrapped = create_regex_nfa(automaton, exp->exp());
             drop_config(wrapped);
 
             epsilon_arc(automaton, start, wrapped.start);
@@ -115,7 +128,7 @@ static INfa create_regex_automaton(
     return { start, end };
 }
 
-static Either<StringView, nfa::Automaton> create_automaton(
+static Either<StringView, nfa::Automaton> create_nfa(
     core::Arena &arena, const Specification::TokenSpec &token)
 {
     using Result = Either<StringView, nfa::Automaton>;
@@ -150,13 +163,180 @@ static Either<StringView, nfa::Automaton> create_automaton(
                 return Result::left(std::move(either_regex.release_left()));
 
             auto regex = either_regex.right();
-            create_regex_automaton(automaton, regex);
+            create_regex_nfa(automaton, regex);
             return Result::right(std::move(automaton));
         }
 
         case Specification::TokenSpec::Type::Invalid:
         default: assert(false and "Unreachable"); return Result::left({});
     }
+}
+
+struct NfaState
+{
+    nfa::Automaton *nfa { nullptr };
+    nfa::State *state { nullptr };
+
+    bool operator==(const NfaState &other) const
+    {
+        return nfa == other.nfa and state == other.state;
+    }
+};
+
+struct DfaState
+{
+    Set<NfaState> nfa_states;
+    nfa::State *dfa_state;
+};
+
+template<typename Callback>
+static void foreach_reachable(
+    nfa::Automaton &automaton, nfa::State *state, Callback callback)
+{
+    for (auto arc : automaton.arcs()) {
+        if (arc->origin == state)
+            callback(*arc);
+    }
+}
+
+static Set<NfaState> reachable_by_epsilon(const Set<NfaState> &states)
+{
+    Set<NfaState> result;
+    for (auto state : states) result.add(state);
+
+    bool modified = true;
+
+    while (modified) {
+        Set<NfaState> reachable;
+
+        for (auto nfa_state : result) {
+            auto &nfa = *nfa_state.nfa;
+
+            foreach_reachable(nfa, nfa_state.state, [&](nfa::Arc &arc) {
+                assert(arc.char_set.non_empty() ^ arc.epsilon);
+
+                if (not arc.epsilon)
+                    return;
+
+                NfaState nfa_state { &nfa, arc.target };
+                reachable.add(std::move(nfa_state));
+            });
+        }
+
+        modified = false;
+        for (auto elem : reachable) {
+            if (not result.contains(elem)) {
+                result.add(std::move(elem));
+                modified = true;
+            }
+        }
+    }
+
+    return result;
+}
+
+static Set<NfaState> reachable_by_char(const Set<NfaState> &states, u8 c)
+{
+    Set<NfaState> reachable;
+
+    for (auto &state : states) {
+        auto &nfa = *state.nfa;
+
+        foreach_reachable(nfa, state.state, [&](nfa::Arc &arc) {
+            assert(arc.char_set.non_empty() ^ arc.epsilon);
+
+            if (arc.epsilon)
+                return;
+            if (not arc.char_set.contains(c))
+                return;
+
+            NfaState nfa_state { &nfa, arc.target };
+            reachable.add(std::move(nfa_state));
+        });
+    }
+
+    return reachable;
+}
+
+static DfaState *dfa_start_state(
+    nfa::Automaton &dfa,
+    Map<Set<NfaState>, DfaState *> &mapping,
+    const List<nfa::Automaton> &nfas)
+{
+    Set<NfaState> nfa_start_states;
+    for (auto &nfa : nfas) {
+        NfaState nfa_state { &nfa, start_state(nfa) };
+        nfa_start_states.add(std::move(nfa_state));
+    }
+
+    auto nfa_states = reachable_by_epsilon(nfa_start_states);
+    if (not mapping.contains(nfa_states)) {
+        auto state = dfa.create_state();
+        state->start = true;
+        auto dfa_state = new DfaState { nfa_states, state };  // @TODO: Arena?
+        mapping.add(nfa_states, dfa_state);
+    }
+    return mapping.get(nfa_states);
+}
+
+static nfa::Automaton create_dfa(
+    core::Arena &arena, const List<nfa::Automaton> &nfas)
+{
+    nfa::Automaton dfa(arena);
+    Map<Set<NfaState>, DfaState *> mapping;
+    List<DfaState *> dfa_state_queue;
+
+    auto *dfa_start = dfa_start_state(dfa, mapping, nfas);
+    dfa_state_queue.add(dfa_start);
+
+    for (Index i = 0; i < dfa_state_queue.size(); ++i) {
+        auto dfa_state = dfa_state_queue[i];
+
+        for (auto c = CharSet::first; c <= CharSet::last; ++c) {
+            auto reachable = reachable_by_epsilon(
+                reachable_by_char(dfa_state->nfa_states, c));
+
+            if (not mapping.contains(reachable)) {
+                auto state = dfa.create_state();
+                auto new_state =
+                    new DfaState { reachable, state };  // @TODO: Arena?
+                dfa_state_queue.add(new_state);
+                mapping.add(reachable, new_state);
+            }
+            auto new_state = mapping.get(reachable);
+
+            nfa::Arc *arc_between = nullptr;
+            for (auto arc : dfa.arcs()) {
+                if (arc->origin == dfa_state->dfa_state and
+                    arc->target == new_state->dfa_state) {
+                    arc_between = arc;
+                    break;
+                }
+            }
+            if (arc_between == nullptr) {
+                arc_between =
+                    dfa.create_arc(dfa_state->dfa_state, new_state->dfa_state);
+            }
+            arc_between->char_set.set(c, true);
+        }
+
+        // @TODO: Different data structure for dfa?
+        // @TODO: Flag explicit error state as error state (iff `reachable` is empty)
+        // @TODO: Label accepting states (Remember which token they represent)
+        // @TODO: Optimize allocations e.g. using Arena?
+        // @TODO: Optimize Set and Map
+        // @TODO: Unit-testing nfa?, dfa
+        bool contains_accepting = false;
+        for (const auto &nfa_state : dfa_state->nfa_states) {
+            if (nfa_state.state->accepting) {
+                contains_accepting = true;
+                break;
+            }
+        }
+        dfa_state->dfa_state->accepting = contains_accepting;
+    }
+
+    return dfa;
 }
 
 Either<StringView, Grammar> sigil::Grammar::compile(
@@ -167,20 +347,24 @@ Either<StringView, Grammar> sigil::Grammar::compile(
 
     List<nfa::Automaton> nfas(specification.tokens().size());
     for (Index i = 0; i < specification.tokens().size(); ++i) {
-        auto either_automaton =
-            create_automaton(arena, specification.tokens()[i]);
+        auto either_automaton = create_nfa(arena, specification.tokens()[i]);
         if (not either_automaton.isRight())
             return Result::left(std::move(either_automaton.release_left()));
 
         auto automaton = std::move(either_automaton.release_right());
 
-        // @TODO: Automaton can't be copied! Is this our Lists fault?
-        core::StringBuilder builder;
-        nfa::Automaton::format(builder, automaton);
-        debug_log(builder.toString(), "\n");
+        //        // @TODO: Automaton can't be copied! Is this our Lists fault?
+        //        core::StringBuilder builder;
+        //        nfa::Automaton::format(builder, automaton);
+        //        debug_log(builder.toString(), "\n");
 
         nfas.add(std::move(automaton));
     }
+
+    auto dfa = create_dfa(arena, nfas);
+    core::StringBuilder builder;
+    nfa::Automaton::format(builder, dfa);
+    debug_log(builder.toString(), "\n");
 
     Grammar grammar;
     return Either<StringView, Grammar>::right(std::move(grammar));
