a863b4574bed10db37d64b1433fffa112a34f215
keddelzz <keddelzz@web.de>
2022-11-25 19:33:27 +0100

    Allow parsing ! on top level and in classes

--- src/RegexParser.cpp
+++ src/RegexParser.cpp
@@ -218,7 +218,8 @@ inline static bool can_be_top_level_atom(u8 c)
         case '^':
         case '$':
         case '%':
-        case '&': return true;
+        case '&':
+        case '!': return true;
         default: return false;
     }
 }
@@ -249,6 +250,10 @@ CharSet RegexParser::parse_top_level_chars()
         advance();  // '&'
         return CharSet('&');
     }
+    if (peek() == '!') {
+        advance();  // '!'
+        return CharSet('!');
+    }
 
     if (peek() == '\\') {
         advance();  // '\\'
@@ -273,6 +278,7 @@ CharSet RegexParser::parse_top_level_chars()
             case '%': return CharSet('%');
             case '&': return CharSet('&');
             case '+': return CharSet('+');
+            case '!': return CharSet('!');
             case 'd': return Digit;
             case 'D': return ~Digit;
             case 'w': return Word;
@@ -323,7 +329,8 @@ inline static bool can_be_class_atom(u8 c)
         case '$':
         case '%':
         case '&':
-        case '+': return true;
+        case '+':
+        case '!': return true;
         default: return false;
     }
 }
@@ -370,6 +377,10 @@ CharSet RegexParser::parse_class_chars()
             advance();  // '+'
             return Result('+');
         }
+        if (peek() == '!') {
+            advance();  // '!'
+            return Result('!');
+        }
 
         if (peek() == '\\') {
             advance();  // '\\'
@@ -389,6 +400,7 @@ CharSet RegexParser::parse_class_chars()
                 case '%': return Result('%');
                 case '&': return Result('&');
                 case '+': return Result('+');
+                case '!': return Result('!');
                 case 'd':
                 case 'D':
                 case 'w':
